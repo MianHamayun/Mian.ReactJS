f73f215219e8fb4a806ee88d7e1141b4868ac553
mianhamayun <mianhamayun9@gmail.com>
2023-11-05 14:55:43 +0500

    part 3 useLoaderData implementation

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/hiteshchoudhary")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+  const data = useLoaderData()
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/mianhamayun")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     });
+  // }, []);
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
       Github Followers: {data.followers}
+      <img className="text-center" src={data.avatar_url} alt="Git Picture" />
     </div>
   );
 }
 
 export default Github;
+
+export const githubloader = async () => {
+  const response = await fetch("https://api.github.com/users/mianhamayun");
+  return response.json();
+};
